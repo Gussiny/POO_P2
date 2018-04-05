d013671f1134360af03cb144acbf4dd54a4f9fb4
FaustoRodriguez <31485146+FaustoRodriguez@users.noreply.github.com>
2018-04-05 10:24:04 -0500

    Add files via upload

--- TextFieldNumeros.java
+++ TextFieldNumeros.java
@@ -1,5 +1,31 @@
+import java.awt.Color;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.JLabel;
 import javax.swing.JTextField;
 
-public class TextFieldNumeros extends JTextField {
-
+public class TextFieldNumeros extends JTextField{
+	private JLabel labelAdvertencia;
+	
+	public TextFieldNumeros(JLabel labelAdvertencia) {
+		super(15);
+		this.labelAdvertencia=labelAdvertencia;
+		this.labelAdvertencia.setForeground(Color.RED);
+		this.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				
+				try{
+					Double.parseDouble(TextFieldNumeros.this.getText());
+					TextFieldNumeros.this.labelAdvertencia.setText("");
+				}
+				catch(NumberFormatException nfe){
+					TextFieldNumeros.this.labelAdvertencia.setText("Ingrese solo numeros");;
+				}
+			}
+		});
+		
+	}
 }
